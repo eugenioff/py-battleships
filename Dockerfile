b9fc1c5621957cc35803185c1aa8c60e7600662c
eugenioff <eugenio.f.1996@gmail.com>
2026-07-04 18:38:16 +0200

    feat: added first py code test

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.14
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD [
+    "uvicorn",
+    "main:app",
+    "--host",
+    "0.0.0.0",
+    "--port"
+    "8081"
+]
---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+from fastapi import FastAPI, Header, HTTPException
+
+app = FastAPI()
+
+EXPECTED_TOKEN = os.environ["AUTH_TOKEN"]
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.get("/secure")
+def secure(authorization: str | None = Header(default=None)):
+    if authorization != f"Bearer {EXPECTED_TOKEN}":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    return {
+        "message": "Authenticated"
+    }
